861654daf7130bc683786b348abed02a8c8b18e9
Jacob Goodall <jraygoodall@gmail.com>
2025-06-02 13:20:16 -0500

    inital commit

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
