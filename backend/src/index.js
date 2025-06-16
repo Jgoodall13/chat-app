c2eb457e32b7d2955da3ed7e1e9e16a6bc4ae10e
Jacob Goodall <jraygoodall@gmail.com>
2025-06-15 19:16:41 -0500

    ts frontend, and login server logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
